775b8da398c2d94aa643553093984e8a23f274f0
Jogvan <jogvan@gmail.com>
2022-04-11 16:22:40 +0100

    Setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    entry: {
+        bundle: path.resolve(__dirname, "src/index.js"),
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name][contenthash].js",
+        clean: true,
+        assetModuleFilename: "[name][ext]",
+    },
+    devtool: "source-map",
+    devServer: {
+        static: {
+          directory: path.resolve(__dirname, "dist"),
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+      },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Restaurant Page",
+            filename: "index.html",
+            template: "src/template.html",
+        }),
+    ],
+}
